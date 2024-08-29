b779d66a452920d90d18fe87664dffa2522ef040
aragakerubo <aragakerubo@gmail.com>
2024-08-28 21:17:51 -0400

    2. Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -26,6 +26,7 @@ def get_locale() -> str:
     """Get locale from request"""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
+
 @app.route("/")
 def index() -> str:
     """Return index page"""
