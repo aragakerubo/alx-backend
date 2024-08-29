00cdc6fdbd20f41016e810ccb5528d9054d83f8d
aragakerubo <aragakerubo@gmail.com>
2024-08-29 00:47:11 -0400

    3. Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -28,7 +28,7 @@ def get_locale() -> str:
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-@app.route("/", methods=["GET"], strict_slashes=False)
+@app.route("/")
 def index() -> str:
     """Return index page"""
     return render_template("3-index.html")
