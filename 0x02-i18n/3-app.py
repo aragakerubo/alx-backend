3a71f09a6c46694e9f13a93535a150c3f4ecf7ac
aragakerubo <aragakerubo@gmail.com>
2024-08-28 23:21:38 -0400

    3. Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,37 @@
+"""
+Basic Flask app, Basic Babel setup, Get locale from request,
+Parameterize templates
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+from typing import List
+
+class Config:
+    """Config app class"""
+
+    LANGUAGES: List[str] = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE: str = "en"
+    BABEL_DEFAULT_TIMEZONE: str = "UTC"
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+@babel.localeselector
+def get_locale() -> str:
+    """Get locale from request"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index() -> str:
+    """Return index page"""
+    return render_template(
+        "3-index.html", title=_("home_title"), header=_("home_header")
+        )
+
+
+if __name__ == "__main__":
+    """ Main Function running in debug mode"""
+    app.run(debug=True)
