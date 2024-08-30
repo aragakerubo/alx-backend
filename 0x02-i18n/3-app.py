1d29db13837211cc4671f799e2334cf61bbcb169
aragakerubo <aragakerubo@gmail.com>
2024-08-30 02:21:44 -0400

    3. Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -32,9 +32,7 @@ def get_locale() -> str:
 @app.route("/")
 def index() -> str:
     """Return index page"""
-    return render_template(
-        "3-index.html", title=_('home_title'), header=_('home_header')
-        )
+    return render_template("3-index.html")
 
 
 if __name__ == "__main__":
